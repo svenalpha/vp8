1f097a49b20b9d387c48bc379e99bc8f26983c48
svenalpha <svalphaforsale@yahoo.com>
2023-04-26 18:41:10 +0700

    1st commit to gh-pages 26/04/2023  18:40

---
+++ dist/server/entry-server.js
@@ -0,0 +1,24 @@
+const javascriptLogo = "/assets/javascript-8dac5379.svg";
+function render() {
+  const html = `
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+        <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+      </a>
+      <h1>Hello preview xyz  Vite!</h1>
+      <div class="card">
+        <button id="counter" type="button"></button>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite logo to learn more
+      </p>
+    </div>
+  `;
+  return { html };
+}
+export {
+  render
+};
